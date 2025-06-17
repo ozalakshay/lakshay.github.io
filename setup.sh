e1e7f28ffd1ef96fb333c123035ae49dad576cf5
Lakshay Oza <86004857+ozalakshay@users.noreply.github.com>
2025-06-17 13:40:25 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 apt update -y
 apt install nginx -y
 
-wget https://raw.githubusercontent.com/YOUR_GITHUB_USERNAME/YOUR_REPO_NAME/main/index.html -O /var/www/html/index.html
+wget https://raw.githubusercontent.com/ozalakshay/lakshay-resume/main/index.html -O /var/www/html/index.html
 
 systemctl enable nginx
 systemctl restart nginx
